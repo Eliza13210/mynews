042e6651f38ddaf5e7ee638f9fc31c4ab8826711
Eliza13210 <elisabetboulanger@gmail.com>
2018-12-13 15:41:56 +0100

    Trying out a way to test my future RXJava stream

--- app/src/test/java/com/oc/liza/mynews/ExampleUnitTest.java
+++ app/src/test/java/com/oc/liza/mynews/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.oc.liza.mynews;
 
 import org.junit.Test;
 
+
+import io.reactivex.observers.TestObserver;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +16,23 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void FetchStreamTopStories() throws Exception {
+        //1 - Get the stream
+        Observable<NewsObject> observableUsers = NewsStream.streamFetchNews("https://api.nytimes.com/svc/topstories/v2/home.json?&amp;api-key=799e9f0e6e264b3a8e21b57f3f05dfd0");
+        //2 - Create a new TestObserver
+        TestObserver<NewsObject> testObserver = new TestObserver<>();
+        //3 - Launch observable
+        observableUsers.subscribeWith(testObserver)
+                .assertNoErrors() // 3.1 - Check if no errors
+                .assertNoTimeout() // 3.2 - Check if no Timeout
+                .awaitTerminalEvent(); // 3.3 - Await the stream terminated before continue
+
+        // 4 - Get list of user fetched
+        NewsObject newsFetched = testObserver.getStatus();
+
+        // 5 - Verify if Jake Wharton follows only 12 users...
+        assertThat("Ok",newsFetched.getStatus() == "Ok");
     }
 }
