43e561d14c6764b371b41a240739a89743fe02eb
Eliza13210 <elisabetboulanger@gmail.com>
2018-12-15 17:34:59 +0100

    Created layout including the image

--- app/src/main/java/com/oc/liza/mynews/view/NewsViewHolder.java
+++ app/src/main/java/com/oc/liza/mynews/view/NewsViewHolder.java
@@ -2,6 +2,7 @@ package com.oc.liza.mynews.view;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -35,12 +36,16 @@ public class NewsViewHolder extends RecyclerView.ViewHolder {
     public void updateWithNewsItem(News newsItem, Context context) {
         this.title.setText(newsItem.getTitle());
         this.date.setText(newsItem.getPublished_date());
-        this.section.setText(newsItem.getSection() + " > " + newsItem.getSubsection());
-        List<NewsImage> images=new ArrayList<>();
-        images.addAll(newsItem.getMultimedia());
-        String url=images.get(0).getUrl();
-        Glide.with(context)
-                .load(url)
-                .into(thumbnail);
+        try {
+            this.section.setText(newsItem.getSection() + " > " + newsItem.getSubsection());
+            List<NewsImage> images = newsItem.getMultimedia();
+            String url = images.get(0).getUrl();
+            Glide.with(context)
+                    .load(url)
+                    .into(thumbnail);
+        }catch (Exception e) {
+            e.printStackTrace();
+            Log.e("exception", "error " + e);
+        }
     }
 }
