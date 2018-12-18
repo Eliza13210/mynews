fcfbd0143c5fde9d0071797d5ae49f0af4abea2f
Eliza13210 <elisabetboulanger@gmail.com>
2018-12-18 14:43:23 +0100

    added travis CI

--- app/src/main/java/com/oc/liza/mynews/controler/activities/SearchActivity.java
+++ app/src/main/java/com/oc/liza/mynews/controler/activities/SearchActivity.java
@@ -5,18 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.oc.liza.mynews.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SearchActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.search_button)
+    Button searchButton;
+    @BindView(R.id.input_query)
+    EditText textEdit;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+        initLayout();
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    private void initLayout(){
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -25,6 +39,13 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               
             }
         });
     }
